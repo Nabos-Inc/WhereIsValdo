f465e755a6fcf67eb917e6f63895c0c96eebe1c7
jose <josmontenegro2012@gmail.com>
2021-01-28 23:48:58 -0500

    Resolution Setup. Character animation base

---
+++ Assets/Scripts/LayeredCharacterAnimator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayeredCharacterAnimator : MonoBehaviour {
+    public List<Animator> animators;
+
+    public void SetFloat(string name, float value) {
+        foreach(Animator anim in animators) {
+            anim.SetFloat(name, value);
+        }
+    }
+}
