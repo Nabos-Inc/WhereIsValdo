d45a74401cf6a4d1959a7023d1b183aba25215ca
Manuel Pacheco <mpachecom@bancofalabella.cl>
2021-01-30 18:26:11 -0300

    add example navagent destination targeting script

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+  public Transform goal;
+  public NavMeshAgent agent;
+
+  void Start()
+  {
+    agent = GetComponent<NavMeshAgent>();
+    agent.destination = goal.position;
+  }
+
+  void Update()
+  {
+    var diff = (agent.destination - goal.position).magnitude;
+    // Debug.Log(diff);
+
+    if (diff > 0.1)
+    {
+      Debug.Log("called!");
+      agent.destination = goal.position;
+    }
+  }
+}
