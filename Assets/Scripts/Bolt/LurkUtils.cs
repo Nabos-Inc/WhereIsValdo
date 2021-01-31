79763008a6be08db07cdeebf3ca766e21ff105f1
Manuel Pacheco <mpachecom@bancofalabella.cl>
2021-01-31 16:16:32 -0300

    add character facing when moving

--- Assets/Scripts/Bolt/LurkUtils.cs
+++ Assets/Scripts/Bolt/LurkUtils.cs
@@ -22,4 +22,34 @@ public class LurkUtils : MonoBehaviour
   {
     return agent.GetComponent<NavMeshAgent>().remainingDistance < 0.2;
   }
+
+  public void updateAnimation(NavMeshAgent agent, CharacterBehaviour character)
+  {
+    var vector = Vector3.ProjectOnPlane(agent.velocity, Vector3.back);
+    if (vector.Equals(Vector3.zero)) return;
+
+    var isHorizontal = Mathf.Abs(vector.x) >= Mathf.Abs(vector.y);
+
+    character.speed = 1f;
+    if (isHorizontal)
+    {
+      character.dirX = vector.x >= 0 ? 1 : -1;
+      character.dirY = 0;
+    }
+    else
+    {
+      character.dirX = 0;
+      character.dirY = vector.y >= 0 ? 1 : -1;
+    }
+  }
+
+  public void stopAnimation(CharacterBehaviour character)
+  {
+    character.speed = 0f;
+
+    // If this is not done, the character stays looking where it moved to
+    // which is better obviously
+    // character.dirX = 0;
+    // character.dirY = 0;
+  }
 }
